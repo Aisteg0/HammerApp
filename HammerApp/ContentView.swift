ebc782e4d8931566b9926284d5e5143aabe79d24
Misha <Mishka.ganin@mail.ru>
2025-07-23 16:40:54 +0300

    init

---
+++ HammerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HammerApp
+//
+//  Created by Михаил Ганин on 23.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HammerApp/HammerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HammerAppApp.swift
+//  HammerApp
+//
+//  Created by Михаил Ганин on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HammerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
